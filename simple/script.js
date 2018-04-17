c04f1620fff9cc365ddc56055e499900e33ce50f
我是阿喵酱 <woshiamiaojiang@163.com>
2018-04-17 20:29:39 +0800

    Update script.js

--- simple/script.js
+++ simple/script.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
-	//var QRBox	=	$('#QRBox');
-	//var MainBox	=	$('#MainBox');
-	//var BTCQR	=	'images/BTCQR.png';	// 二维码路径
+	var QRBox	=	$('#QRBox');
+	var MainBox	=	$('#MainBox');
+	var BTCQR	=	'images/BTCQR.png';	// 二维码路径
 	var AliPayQR	=	'images/AliPayQR.png';
 	var WeChanQR	=	'images/WeChatQR.png';
 
